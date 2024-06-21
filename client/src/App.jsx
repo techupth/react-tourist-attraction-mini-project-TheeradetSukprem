5940a5d3530374b4e8d43dada21217d7cf05c024
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-06-21 14:34:44 +0700

    Feat : Move the line in the app page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
   <div className="App">
     <Header />
-    {/* <SearchTopic />  สร้างแยกแล้วไม่สามารถใช้งานได้เลยไม่นำมาใช้  */}
     <TopicHighlight />
+    {/* <SearchTopic />  สร้างแยกแล้วไม่สามารถใช้งานได้เลยไม่นำมาใช้  */}
   </div>
   )
 };
